357a5b99d4fe20f59bbc1237ffc01639c40f20ca
Kathy Bauer <kbauer17@my.wctc.edu>
2024-01-23 08:24:20 -0600

    [1.04] update Country.jsx - add functions for buttons - add styling to buttons (added Bootstrap to index.js)

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -2,13 +2,31 @@ import React, { Component } from 'react';
 
 class Country extends Component {
     state = {
-        name: 'Mexico',
+        name: 'United States',
         gold: 0,
     }
 
+    handleIncrement = () => {
+        this.setState({ gold: this.state.gold + 1 })
+    };
+
+    handleDecrement = () => {
+        this.setState({ gold: this.state.gold - 1 })
+    };
+    
     render(){
         return (
-            <div>{ this.state.name }</div>
+            <div>
+                <div className='m-3'>{ this.state.name }</div>
+                <span className='m-3'> Gold medals:  { this.state.gold }
+                    <div className='btn-group px-3' role='group'>
+                        <button onClick={ this.handleIncrement } className="btn btn-outline-primary btn-sm"> + </button>
+                        <button onClick={ this.handleDecrement } className="btn btn-outline-primary btn-sm"> - </button>
+                    </div> 
+                    
+                </span>
+            </div>
+            
         )
     }
 }
